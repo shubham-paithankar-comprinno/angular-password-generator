a40c5c1543391c1bc681f2115f31848926d5c3af
RYUU <shubham.paithankar@comprinno.net>
2021-12-07 11:27:23 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  passwordLength = Boolean(false)
+  length = 1
+  password = ''
+
+  //Booleans
+  includeLetters = Boolean(false)
+  includeNumbers = Boolean(false)
+  includeSymbols = Boolean(false)
+
+  //Functions
+  
+  onLengthChange (E: Event) {
+    let { value } = E.target as HTMLButtonElement
+    if (!isNaN(parseInt(value))) {
+      this.length = parseInt(value) 
+      this.passwordLength = Boolean(true)
+    } else {
+      this.passwordLength = Boolean(false)
+    }
+  }
+
+  useLetters () {
+    this.includeLetters = !this.includeLetters
+  }
+
+  useNumbers () {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  useSymbols () {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onButtonClick () {
+    const numbers = '1234567890'
+    const letters = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVQWXYZ'
+    const symbols = `!@#$%^&*()`
+
+    let validChars = ``
+    this.includeLetters ? validChars += letters : null
+    this.includeNumbers ? validChars += numbers : null
+    this.includeSymbols ? validChars += symbols : null
+
+    let generatedPassword = ''
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      generatedPassword += validChars[index]  
+    }
+    this.password = generatedPassword
+  }
 }
